a18ba22b465a8bdcb4bd7fd73e82fb0f42107034
Lucie <chimitslucie@gmail.com>
2023-11-18 10:36:21 +0100

    Création de l'index

--- Frontend/argentbank/src/index.js
+++ Frontend/argentbank/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/main.css"; // Importation du fichier CSS principal
+import App from "./App"; // Importation du composant racine de l'application
+import { BrowserRouter } from "react-router-dom"; // Importation de BrowserRouter pour gérer les routes
+import store from "./Redux/store"; // Importation du store Redux
+import { Provider } from "react-redux"; // Utilisation du composant Provider pour connecter l'application au store Redux
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Création de la racine (root) pour l'application React
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Rendu de l'application React dans la racine
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <div className="main-container">
+      {/* Utilisation du BrowserRouter pour gérer les routes de l'application */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </div>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
