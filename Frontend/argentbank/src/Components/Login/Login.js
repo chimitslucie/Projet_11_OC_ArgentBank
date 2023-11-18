0c8de12ff3807247b60b97e7d7d62d6f8b0cd1f5
Lucie <chimitslucie@gmail.com>
2023-11-18 11:31:41 +0100

    Création page login

---
+++ Frontend/argentbank/src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../Form/Form";
+
+function Login() {
+  return (
+    <div className="bg-dark">
+      <div className="Formulaire"></div>
+      <Form />
+    </div>
+  );
+}
+
+export default Login;
